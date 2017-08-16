2fe2886cc978e950f365b36ca3c887c7c7b0c3eb
Matt Parkes <mattparkes@null.net>
2017-08-16 11:51:04 +1000

    Initial code:  - Get-StatusPage  - Get-StatusPageIncident

---
+++ PoSh-StatusPageIO.psm1
@@ -0,0 +1,100 @@
+﻿Function Invoke-StatusPageRequest {
+    [CmdletBinding(DefaultParameterSetName="Url")]
+    Param(
+        [Parameter(Mandatory=$True, ParameterSetName='Id')][String]$Id,
+        [Parameter(Mandatory=$True, ParameterSetName='Url')][String]$Url,
+        [Parameter(Mandatory=$False)][Int]$APIVersion = 2,
+        [Parameter(Mandatory=$False)][String]$Endpoint = "summary.json"
+    )
+
+    # Build the BaseURL for the API Requests, either from the ID or from the URL Supplied
+    [String]$BaseUrl = ""
+    If ($PSCmdlet.ParameterSetName -Eq "Id") {
+        $BaseUrl = "$Id.statuspage.io"
+    }
+    ElseIf ($PSCmdlet.ParameterSetName -Eq "Url") {
+        $BaseUrl = $Url
+        $BaseUrl = $BaseUrl.TrimEnd("/")
+    }
+
+    $Output = [Ordered]@{}
+    $StatusPageWebRequest = @{}
+
+    $StatusPageWebRequest = Invoke-WebRequest -DisableKeepAlive -UseBasicParsing -TimeoutSec 30 -Uri "$BaseUrl/api/v$APIVersion/$Endpoint"
+
+    Return $StatusPageWebRequest
+}
+
+Function Get-StatusPage{
+    [CmdletBinding(DefaultParameterSetName="Url")]
+    Param(
+        [Parameter(Mandatory=$True, ParameterSetName='Id', ValueFromPipeline=$True)][String]$Id,
+        [Parameter(Mandatory=$True, ParameterSetName='Url',   ValueFromPipeline=$True)][String]$Url
+    )
+
+    $StatusPageWebRequest = Invoke-StatusPageRequest @PSBoundParameters -Endpoint "summary.json"
+
+    If ($StatusPageWebRequest.StatusCode -Eq 200) {
+        $StatusPage = ($StatusPageWebRequest.Content | ConvertFrom-Json)
+
+        # Clean up the output. This is unnecessary, but if we use CamelCase keys and set some Types, it feels more PowerShell-esque.
+        $Output = [ordered]@{
+            "Id" = [String]($StatusPage.page.id);
+            "Name" = [String]($StatusPage.page.name);
+            "Url" = [String]($StatusPage.page.url);
+            "Updated" = [DateTime]($StatusPage.page.updated_at);
+            "StatusDescription" = [String]($StatusPage.status.description);
+            "StatusIndicator" = [String]($StatusPage.status.indicator);
+            "Components" = [Array]@();
+        };
+        $StatusPage.components | ForEach-Object {
+            $Output.Components += [ordered]@{
+                "Id" = [String]($_.id)
+                "Name" = [String]($_.name)
+                "Description" = [String]($_.description)
+                "Status" = [String]($_.status)
+                "CreatedAt" = [DateTime]($_.created_at)
+                "UpdatedAt" = [DateTime]($_.updated_at)
+                "Position" = [Int]($_.position)
+                "Showcase" = [Boolean]($_.showcase)
+                "GroupId" = [String]($_.group_id)
+                "PageId" = [String]($_.page_id)
+                "Group" = [String]($_.group)
+                "OnlyShowIfDegraded" = [Boolean]($_.only_show_if_degraded)
+            }
+        }
+
+        Return $Output
+    }
+}
+
+Function Get-StatusPageIncident{
+    [CmdletBinding(DefaultParameterSetName="Url")]
+    Param(
+        #TODO: Change ID to StatusPageId and URL to StatusPageURL
+        [Parameter(Mandatory=$True, ParameterSetName='Id', ValueFromPipeline=$True)][String]$Id,
+        [Parameter(Mandatory=$True, ParameterSetName='Url',   ValueFromPipeline=$True)][String]$Url,
+        [Parameter(Mandatory=$False, ValueFromPipeline=$True)][Switch]$All
+        #TODO: Add IncidentID (or just ID)
+        #TODO: ParameterSet/Alias allowing you to feed in a StatusPage object
+        
+    )
+
+    $StatusPageWebRequest = @{}
+    If ($All) {
+        $BoundParameters = $PSBoundParameters
+        $BoundParameters.Remove('All')
+        $StatusPageWebRequest = Invoke-StatusPageRequest @BoundParameters -Endpoint "/incidents.json"    
+    }
+    Else {
+        $StatusPageWebRequest = Invoke-StatusPageRequest @PSBoundParameters -Endpoint "/incidents/unresolved.json"    
+    }
+    
+    If ($StatusPageWebRequest.StatusCode -Eq 200) {
+        $Incidents = ($StatusPageWebRequest.Content | ConvertFrom-Json)
+        
+        Return $Incidents.incidents
+    }
+}
+
+Export-ModuleMember *-*
--- README.md
+++ README.md
@@ -1,2 +1,35 @@
-# PoSh-StatusPageIO
-PowerShell wrappers for the statuspage.io API
+# PoSh-StatusPageIO:
+PowerShell cmdlets for querying a status page running on statuspage.io.
+Currently only supports reading data, but will be updated to allow you to POST changes to your own Status Page in the future.
+
+### Cmdlets:
+
+  - `Get-StatusPage` returns a summary off the status page
+  - `Get-StatusPageIncident` returns a list of all the urnesolved inceidents. Providing the `-All` switch instead returns all incidents (resolved and unresolved).
+
+### Examples:
+
+Get the summary of Kickstarter's Status Page via URL:
+```
+Get-StatusPage -Url status.kickstarter.com
+```
+
+Get the summary of Kickstarter's Status Page using it's ID/Page Code:
+```
+Get-StatusPage -Id 4p1vb67yqzdy
+```
+
+Get any unresolved incidents from Kickstarter's Status Page:
+```
+Get-StatusPageIncident -Url status.kickstarter.com
+```
+
+Get _all_ resolved and unresolved incidents from Kickstarter's Status Page:
+```
+Get-StatusPageIncident -Url status.kickstarter.com -All
+```
+
+### FAQ:
+
+##### Q: Can't I just do this myself with Invoke-WebRequest:
+**A:** Yep! This is just a bit cleaner.
